7e8d076af66e55eccf5cddc0191bb5c890ef49f3
Lässe <lars.volkheimer@gmx.ch>
2018-11-09 07:54:46 +0100

    fortmat calculator.java

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -23,18 +23,18 @@ public class Calculator {
 			cnt++;
 		if (widerstand != 0)
 			cnt++;
-		if (cnt<2)
+		if (cnt < 2)
 			warning = "nicht genug Werte";
-		else if (cnt>2)
+		else if (cnt > 2)
 			warning = "zu viele Werte eingegeben";
 		else
 			warning = "";
 
 		if (warning.equals("")) {
-		this.leistung = leistung;
-		this.spannung = spannung;
-		this.strom = strom;
-		this.widerstand = widerstand;
+			this.leistung = leistung;
+			this.spannung = spannung;
+			this.strom = strom;
+			this.widerstand = widerstand;
 		}
 	}
 
@@ -64,24 +64,19 @@ public class Calculator {
 		if (leistung != 0 && spannung != 0) {
 			strom = iAusPundU(leistung, spannung) * -1;
 			widerstand = rAusPundI(leistung, spannung) * -1;
-		}else
-		if (leistung != 0 && strom != 0) {
+		} else if (leistung != 0 && strom != 0) {
 			spannung = uAusPundI(leistung, strom) * -1;
 			widerstand = rAusPundI(leistung, strom) * -1;
-		}else
-		if (leistung != 0 && widerstand != 0) {
+		} else if (leistung != 0 && widerstand != 0) {
 			spannung = uAusPundR(leistung, widerstand) * -1;
 			strom = iAusPundR(leistung, widerstand) * -1;
-		}else
-		if (spannung != 0 && strom != 0) {
+		} else if (spannung != 0 && strom != 0) {
 			leistung = pAusUundI(spannung, strom) * -1;
 			widerstand = rAusUundI(spannung, strom) * -1;
-		}else
-		if (spannung != 0 && widerstand != 0) {
+		} else if (spannung != 0 && widerstand != 0) {
 			leistung = pAusUundR(spannung, widerstand) * -1;
 			strom = iAusUundR(spannung, widerstand) * -1;
-		}else
-		if (widerstand != 0 && strom != 0) {
+		} else if (widerstand != 0 && strom != 0) {
 			leistung = pAusRundI(widerstand, strom) * -1;
 			spannung = uAusRundI(widerstand, strom) * -1;
 		}
@@ -125,14 +120,15 @@ public class Calculator {
 	}
 
 	public double rAusUundI(double u, double i) {
-		return u/i;
+		return u / i;
 	}
 
 	public double rAusPundI(double p, double i) {
-		return p/(i*i);
+		return p / (i * i);
 	}
+
 	public double rAusPundU(double p, double u) {
-		return (u*u)/p;
+		return (u * u) / p;
 	}
 
 	public String getWarning() {
